889fa458b7107e97b633d92c2060e9c4d832f80a
gabrielmmendes <gabrielmendes.0904@gmail.com>
2024-04-26 21:10:40 -0300

    implementação

--- main.go
+++ main.go
@@ -2,13 +2,46 @@ package main
 
 import (
     "net/http"
+    "html/template"
 )
 
 func main() {
-    // Configuração do servidor para servir arquivos estáticos (HTML, CSS, JS, imagens, etc.)
-    fs := http.FileServer(http.Dir("./"))
-    http.Handle("/", fs)
+    http.HandleFunc("/", handler)
 
     // Iniciar o servidor na porta 8080
     http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    tmpl, _ := template.ParseFiles("index.html")
+
+    p := Pacientes{
+        Itens: []Paciente{
+            {
+                Nome:           "Diogo SIlva Lima",
+                Cpf:            "867.420.587-90",
+            },
+            {
+                Nome:           "Diogo SIlva Lima",
+                Cpf:            "867.420.587-90",
+            },
+        },
+    }
+
+    tmpl.Execute(w, p)
+}
+
+type Pacientes struct {
+    Itens []Paciente
+}
+
+type Paciente struct {
+    Nome           string
+    Cpf            string
+    DataNascimento string
+    Telefone       string
+    Sexo           string
+    EstaFumante    bool
+    FazUsoAlcool   bool 
+    SituacaoDeRua  bool
 }
