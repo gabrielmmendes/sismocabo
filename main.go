46913e97af54d8567d53290e09e6e1d1c4525204
gabrielmmendes <gabrielmendes@discente.ufg.br>
2024-05-03 19:55:25 -0300

    conexão com banco de dados e criação de tabela paciente

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"ip-web/infra"
+	"ip-web/model"
 	"net/http"
 	"os"
 )
 
-var templates = template.Must(template.ParseFiles("./index.html", "./templates/dashboard.html", "./templates/mapa.html", "./templates/usuario.html"))
+var templates = template.Must(template.ParseGlob("./templates/*"))
+var index = template.Must(template.ParseFiles("./index.html"));
 
 func main() {
+	infra.CreateConnection()
+
 	http.HandleFunc("/", handler)
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.HandleFunc("/dashboard", dashboard)
@@ -28,7 +33,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	byteJson, _ := io.ReadAll(jsonFile)
 	json.Unmarshal(byteJson, &Pacientes)
 
-	templates.Execute(w, Pacientes)
+	index.Execute(w, Pacientes)
 }
 
 func dashboard(w http.ResponseWriter, r *http.Request) {
@@ -43,19 +48,6 @@ func usuario(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "usuario.html", "a")
 }
 
-type Paciente struct {
-	Nome              string `json:"nome"`
-	Cpf               string `json:"cpf"`
-	DataNascimento    string `json:"data_nasc"`
-	Idade             int 	 `json:"idade"`
-	Telefone          string `json:"celular"`
-	Sexo              string `json:"sexo"`
-	Cep               string `json:"cep"`
-	EstaFumante       bool   `json:"esta_fumante"`
-	FazUsoAlcool      bool   `json:"faz_uso_alcool"`
-	EstaSituacaoDeRua bool   `json:"esta_situacao_de_rua"`
-}
-
 type Pacientes struct {
-	Pacientes []Paciente `json:"pacientes"`
+	Pacientes []model.Paciente `json:"pacientes"`
 }
